8e6081f4a97bbb1f1c464ab3aa0ebb5eb8df682d
River McCubbin <mccubbinri@gmail.com>
2021-03-03 13:23:30 -0500

    added getPrimeFactors page and also update js to be more compatible in general

--- js/calculators.js
+++ js/calculators.js
@@ -1,22 +1,36 @@
-function isPrime() {
-        var num = document.getElementById('num').value
-        var result = document.getElementById('result')
+function isPrime(num=null) {
+        // if num is not declared then assume this is being called by an html page, otherwise run like a normal function
+        if (!num) {
+                var num = document.getElementById('num').value
+                var result = document.getElementById('result')
+        }
+
+        if (num < 2) {
+                if (result) {
+                        result.textContent = "Result: " + num + " is not prime";
+                }
+                return false;
+        }
 
         for (var i = 2; i <= Math.sqrt(num); i++) {
                 if (num % i == 0) {
-                        result.textContent = "Result: " + num + " is not prime";
+                        if (result) {
+                                result.textContent = "Result: " + num + " is not prime";
+                        }
                         return false;
                 }
         }
 
-        result.textContent = "Result: " + num + " is prime!";
+        if (result) {
+                result.textContent = "Result: " + num + " is prime!";
+        }
         return true;
 }
 
 
 function getFactors() {
-        var num = document.getElementById('num').value
-        var result = document.getElementById('result')
+        var num = document.getElementById('num').value;
+        var result = document.getElementById('result');
         var factors = [];
 
         for (var i = 0; i <= num; i++) {
@@ -25,6 +39,23 @@ function getFactors() {
 
         result.textContent = "Result: The factors of " + num + " are " + arrayToString(factors);
         return factors;
+}
+
+
+function getPrimeFactors() {
+        var num = document.getElementById('num').value;
+        var result = document.getElementById('result');
+
+        var factors = getFactors();
+
+        var prime_factors = [];
+
+        for (var i = 0; i < factors.length; i++) {
+                if (isPrime(factors[i])) prime_factors.push(factors[i]);
+        }
+
+        result.textContent = "Result: The prime factors of " + num + " are: " + arrayToString(prime_factors);
+        return prime_factors;
 }
 
 
